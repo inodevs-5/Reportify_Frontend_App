11cda046d11dc557d184cbd0ab8dbb3ce588c2ae
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-04-11 08:51:29 -0300

    Adicionando rotas exclusivas para administrador

--- src/Routes/Stack/admin.routes.tsx
+++ src/Routes/Stack/admin.routes.tsx
@@ -8,7 +8,7 @@ import { propsNavigationStack } from "./Models";
 
 const { Navigator, Screen } = createNativeStackNavigator<propsNavigationStack>()
 
-export default function AuthRoutes () {
+export default function AdminRoutes () {
     return(
         <Navigator initialRouteName="Home"  screenOptions={{headerShown : false}}>
             <Screen name="Home" component={Home}/>
@@ -16,9 +16,11 @@ export default function AuthRoutes () {
             <Screen name="TabelaROs" component={TabelaROs}/>
             {/* 
             <Screen name="EdicaoROs" component={EdicaoROs}/>
-            <Screen name="PerfilUsuario" component={PerfilUsuario}/>
             <Screen name="Chat" component={Chat}/>
-            <Screen name="Notificacoes" component={Notificacoes}/> 
+            <Screen name="Notificacoes" component={Notificacoes}/>
+            <Screen name="PerfilUsuario" component={PerfilUsuario}/>
+            <Screen name="TabelaUsuarios" component={TabelaUsuarios}/>
+            <Screen name="CadastroUsuario" component={CadastroUsuario}/> 
             */}
         </Navigator>
     )
