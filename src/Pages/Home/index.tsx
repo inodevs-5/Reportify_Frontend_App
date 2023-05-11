d86e301ccc76b6b2aa06e5aad9f9d8bb4f6aa711
unknown <juliamaria215@gmail.com>
2023-05-10 23:03:51 -0300

    adicionando icone de perfil corretamente e alinhando-o

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable quotes */
 import React, { useState } from 'react';
 import {StyleSheet,Modal, View,Text,TextInput,TouchableOpacity,Platform, ActivityIndicator} from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons'
+import Icon from 'react-native-vector-icons/Ionicons';
 import { propsStack } from '../../Routes/Stack/Models';
 import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../contexts/auth';
-
+import Icone from 'react-native-vector-icons/FontAwesome';
 export const Home = () =>{
   const { usuario, signOut } = useAuth();
 
@@ -19,27 +19,32 @@ export const Home = () =>{
   }
   return (
     
-    <><View style={style.containericone}>
-      <TouchableOpacity onPress={handlePress}>
-        <Icon name="user-circle" size={50} color="gray" />
-      </TouchableOpacity>
-      <Modal visible={showModal} animationType="slide">
-        <View style={style.modal}>
-          <Text style={style.title}>Informações de perfil</Text>
-          <Text style={style.text}>Nome: João da Silva</Text>
-          <Text style={style.text}>Email: joao.silva@example.com</Text>
-          <Text style={style.text}>Tipo de perfil: Usuário</Text>
-          <TouchableOpacity onPress={() => setShowModal(false)}>
-            <Text style={style.close}>Fechar</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-    </View>
+   
     <View style={style.container}>
         <Text style={style.title}>Olá, {usuario.nome}!</Text>
         <TouchableOpacity onPress={signOut} style={style.exitIcon}>
           <Icon name='exit-outline' size={30} />
         </TouchableOpacity>
+        
+
+        <View style={style.containericone}>
+          <TouchableOpacity onPress={handlePress} style={style.userIcon}>
+            
+            <Icone name='user-circle' size={30}  />
+          </TouchableOpacity>
+          <Modal visible={showModal} animationType="slide">
+            <View style={style.modal}>
+              <Text style={style.title}>Informações de perfil</Text>
+              <Text style={style.text}>Nome: João da Silva</Text>
+              <Text style={style.text}>Email: joao.silva@example.com</Text>
+              <Text style={style.text}>Tipo de perfil: Usuário</Text>
+              <TouchableOpacity onPress={() => setShowModal(false)}>
+                <Text style={style.close}>Fechar</Text>
+              </TouchableOpacity>
+            </View>
+          </Modal>
+        </View>
+
         <View style={style.containerbusca}>
           <View style={style.container12}>
             <TextInput style={style.busca}
@@ -106,15 +111,21 @@ export const Home = () =>{
           </View>
         </View>
 
-      </View></>
+      </View>
   );
 }
 
 const style = StyleSheet.create({
+  
+  close: {
+    fontSize: 16,
+    color: 'blue',
+    textDecorationLine: 'underline',
+  },
   containericone: {
     position: 'absolute',
-    top: 0,
-    right: 0,
+    top: '2.5%',
+    right: '2%',
     padding: 10,
   },
   modal: {
