326cb93e19aa616589cb643e0b2eddf2dd531454
Gustavo Ando <gustavo.k.ando@gmail.com>
2023-06-01 21:26:37 -0300

    Gráfico e filtros funcionais com o back-end

--- src/Pages/Dashboard/index.tsx
+++ src/Pages/Dashboard/index.tsx
@@ -1,23 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import {StyleSheet, View, Text, ScrollView,TouchableOpacity, Alert} from 'react-native';
+import {StyleSheet, View, Text, ScrollView,TouchableOpacity, Alert, ActivityIndicator} from 'react-native';
 import { propsStack } from '../../Routes/Stack/Models';
 import { useNavigation } from '@react-navigation/native';
 import api from '../../services/api';
-import { VictoryPie, VictoryTooltip } from 'victory-native'
+import { VictoryChart, VictoryLine, VictoryPie, VictoryTooltip } from 'victory-native'
 import { Picker } from '@react-native-picker/picker';
+import Menu from '../../components/menu';
 
 export const Dashboard = () =>{
   
-  const navigation = useNavigation<propsStack>();
+  const navigation = useNavigation<propsStack>()
 
-  const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
-  const [data, setData] = useState([]);
-  const [selected, setSelected] = useState('');
-  const [date, setDate] = useState("Janeiro 2023");
+  const [loading, setLoading] = useState(true)
+  const [chartData, setChartData] = useState([])
+  const [chartDataLine, setChartDataLine] = useState([])
+  const [selected, setSelected] = useState('')
+  const [date, setDate] = useState('')
   const [dates, setDates] = useState([])
   const [usuario, setUsuario] = useState()
   const [usuarios, setUsuarios]= useState()
+  const [totalROs, setTotalROS] = useState("")
 
   function clickCard(id: string) {
     setSelected(prev => prev === id ? "" : id)
@@ -26,39 +28,66 @@ export const Dashboard = () =>{
   useEffect(() => {
     (async () => {
       try {
-        const response = await api.get('/dashboard/dates');
-        setDates(response.data);
-        setDate(response.data[0])
+        if (!date) {
+          const response = await api.get('/dashboard/dates');
+          setDates(response.data);
+          setDate(response.data[0])
+        }
 
-        const response2 = await api.get('/usuario');
-        setUsuarios(response2.data)
-        setUsuario(response2.data[0])
-        
-        setLoading(false)
-      } catch (response) {
-        Alert.alert(response.data.msg);
-      }
-    })();
-  }, []);
+        if (!usuario) {
+          const response2 = await api.get('/usuario');
+          const list = response2.data
+          list.unshift({_id: "geral", nome: "Geral"})
+          setUsuarios(list)
+          setUsuario(response2.data[0]._id)
+        }
 
-  useEffect(() => {
-    (async () => {
-      try {
         if (date && usuario) {
-          const response = await api.get('/dashboard/data/' + date + '/' + + usuario._id );
-          setData(response.data);
+          const response3 = await api.get('/dashboard/data/' + date + '/' + usuario );
+          const data = response3.data
+
+          setTotalROS(String(data.total))
+
+          const chartPie = [{
+            id: "1",
+            label: "Sem tratamento",
+            value: data.aberto,
+            color: '#878787'
+          },
+          {
+            id: "2",
+            label: "Em tratamento",
+            value: data.andamento,
+            color: "#72A2FA"
+          },
+          {
+            id: "3",
+            label: "Solucionado",
+            value: data.fechado,
+            color: "#2B3467"
+          }]
+
+          setChartData(chartPie)
+
+          // const response4 = await api.get('/dashboard/chartline/' + date + '/' + usuario );
+          // const data2 = response4.data
+
+          // setChartDataLine({total: data2.total, fechado: data2.fechado})
         }
       } catch (response) {
         Alert.alert(response.data.msg);
       }
+      setLoading(false)
     })();
   }, [date, usuario])
 
   return (
-    <ScrollView>
+    <>
+    <ScrollView contentContainerStyle={{flexGrow: 1}}>
       <View style={style.container}>
         <Text style={style.title}>Dashboard</Text>
-        <View style={style.picker}>
+        {!loading ? <>
+          <View style={style.picker}>
           <Text style={style.subtitle}>Data:</Text>
           <Picker
             selectedValue={date}
@@ -81,16 +110,17 @@ export const Dashboard = () =>{
             }
           </Picker>
         </View>
+
         <View style={style.picker}>
           <Text style={style.subtitle}>Usuário:</Text>
           <Picker
-            selectedValue={date}
-            onValueChange={(itemValue) => setDate(itemValue)}
+            selectedValue={usuario}
+            onValueChange={(itemValue) => setUsuario(itemValue)}
             style={{
               backgroundColor: '#FFF',
               height: 50,
               flex: 1,
-              marginLeft: 28
+              marginLeft: '7%'
             }}
           >
             {
@@ -104,82 +134,145 @@ export const Dashboard = () =>{
             }
           </Picker>
         </View>
+
         <View style={style.chart}>
-          <VictoryPie 
-            data={data}
-            x="label"
-            y="value"
-            colorScale={['#878787', '#72A2FA' , '#2B3467']}
-            innerRadius={75}
-            animate={{
-              duration: 2000,
-              easing: "linear"
-            }}
-            style={{
-                labels: {
-                  fill: 'white'
-                },
-                data: {
-                  fillOpacity: ({datum}) => (datum.id === selected || selected === "") ? 1 : 0.3,
-                  stroke: ({datum}) => (datum.id === selected) ? 'black' : 'none',
-                  strokeWidth: 3,
-                  strokeOpacity: 0.5
-                }
-            }}
-            labelComponent={
-              <VictoryTooltip 
-                renderInPortal={false}
-                flyoutStyle={{
-                  stroke: 0,
-                  fill: ({datum}) => datum.color
-                }}
-              />
-            }
-          />
-          {data &&           
-            <TouchableOpacity style={style.card} onPress={() => clickCard(data[0].id)}>
-              <View style={style.tag1} />
+         {chartData &&
+          (chartData[0].value === 0 && chartData[1].value === 0 && chartData[2].value === 0) ? 
+            <Text style={style.noChart}>Não há nenhum dado de RO para este gráfico.</Text>
+            :
+            <VictoryPie 
+              data={chartData}
+              x="label"
+              y="value"
+              colorScale={['#878787', '#72A2FA' , '#2B3467']}
+              innerRadius={75}
+              animate={{
+                duration: 2000,
+                easing: "linear"
+              }}
+              style={{
+                  labels: {
+                    fill: 'white'
+                  },
+                  data: {
+                    fillOpacity: ({datum}) => (datum.id === selected || selected === "") ? 1 : 0.3,
+                    stroke: ({datum}) => (datum.id === selected) ? 'black' : 'none',
+                    strokeWidth: 3,
+                    strokeOpacity: 0.5
+                  }
+              }}
+              labelComponent={
+                <VictoryTooltip 
+                  renderInPortal={false}
+                  flyoutStyle={{
+                    stroke: 0,
+                    fill: ({datum}) => datum.color
+                  }}
+                />
+              }
+            />
+          }
+
+          {totalROs && 
+            <TouchableOpacity style={style.card} onPress={() => clickCard('')}>
+              <View style={style.tag} />
               <Text style={style.label}>
-                RO's abertos
+                Total de ROs
               </Text>
               <Text style={style.value}>
-                {data.aberto}
+                {totalROs}
               </Text>
             </TouchableOpacity>
           }
 
-          {data &&
-            <TouchableOpacity style={style.card} onPress={() => clickCard(data[1].id)}>
-              <View style={style.tag2} />
+          {chartData[0] &&           
+            <TouchableOpacity style={style.card} onPress={() => clickCard(chartData[0].id)}>
+              <View style={style.tag1} />
               <Text style={style.label}>
-                RO's em andamento
+                RO's sem tratamento
               </Text>
               <Text style={style.value}>
-                {data.andamento}
+                {chartData[0].value}
               </Text>
-           </TouchableOpacity>
+            </TouchableOpacity>
           }
 
-          {data && 
-            <TouchableOpacity style={style.card} onPress={() => clickCard(data[2].id)}>
-              <View style={style.tag3} />
+          {chartData[1] &&
+            <TouchableOpacity style={style.card} onPress={() => clickCard(chartData[1].id)}>
+              <View style={style.tag2} />
               <Text style={style.label}>
-                RO's resolvidos
+                RO's em tratamento
               </Text>
               <Text style={style.value}>
-                {data.fechado}
+                {chartData[1].value}
               </Text>
-          </TouchableOpacity>
+            </TouchableOpacity>
           }
+
+          {chartData[2] && 
+            <TouchableOpacity style={style.card} onPress={() => clickCard(chartData[2].id)}>
+              <View style={style.tag3} />
+              <Text style={style.label}>
+                RO's solucionados
+              </Text>
+              <Text style={style.value}>
+                {chartData[2].value}
+              </Text>
+            </TouchableOpacity>
+          }
+
+          {/* {
+            chartDataLine &&
+            <VictoryChart>
+              <VictoryLine
+                  data={chartDataLine.total}
+                  x="label"
+                  y="value"
+                  style={{
+                    data: {
+                      stroke: "#72A2FA",
+                      strokeWidth: 5
+                    },
+                    labels: {
+                      display: "none"
+                    }
+                  }}
+              />
+              <VictoryLine
+                  data={chartDataLine.fechado}
+                  x="label"
+                  y="value"
+                  style={{
+                    data: {
+                      stroke: "#2B3467",
+                      strokeWidth: 5
+                    },
+                    labels: {
+                      display: "none"
+                    }
+                  }}
+              />
+            </VictoryChart>
+          } */}
         </View>
+        </> : 
+          <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <ActivityIndicator size="large" color="#666"/>
+          </View>
+        }
       </View>
     </ScrollView>
+
+    <View style={{position:'absolute', left: 0, right: 0, bottom: 0, alignItems: 'center'}}>
+        <Menu/>
+      </View>
+    </>
   );
 };
 
 const style = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, 
     padding: 20,
     backgroundColor: '#E1E1EF',
   },
@@ -193,6 +286,13 @@ const style = StyleSheet.create({
     width: '100%',
     alignItems: 'center'
   },
+  noChart: {
+    fontSize: 18,
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    marginVertical: 100,
+  },
   card: {
     width: '100%',
     height: 80,
@@ -201,6 +301,12 @@ const style = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#ffffff',
     overflow: 'hidden'
+  },
+  tag: {
+    width: 10,
+    height: 80,
+    marginRight: 16,
+    backgroundColor: '#ffffff'
   },
   tag1: {
     width: 10,
